f2f406967b3ca738d1d1b14b76daf6296440c1d7
phanee47 <67285782+phanee47@users.noreply.github.com>
2023-06-24 11:30:27 +0530

    Update dockerImageScan.groovy

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -11,6 +11,6 @@ def call(String aws_account_id, String region, String ecr_repoName){
     sh """
     trivy image ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest > scan.txt
     cat scan.txt
-    trivy image --exit-code 1 --severity CRITICAL ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
+    trivy image --exit-code 1 --severity CRITICAL node:16-alpine
     """
 }
